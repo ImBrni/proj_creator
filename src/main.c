fefd19720c73de3d26d8a29a2b9ddf9b84e56d53
Barney <gepkiraly@gmail.com>
2024-04-16 10:58:37 +0200

    Proejct done, now will try to add self replication with new templates

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #define PROGRAM_NAME "proj_creator"
 #define VERSION 0.1
@@ -11,12 +12,15 @@ int minor_version = 0;
 struct Template {
 	char* name;
 	char* desc;
+	char* file_name;
 	char* project_file;
 };
 
-struct Template templates[] = {
-	{"c", "Starting c project file","this is a c file"},
-	{"bash", "bash script starter","this is a bash file"},
+const struct Template templates[] = {
+	{"c", "Starting c project file", "main.c", "#include <stdio.h>\n\nint main(void) {\n\n\tprintf(\"Hello World!\\n\");\n\n\treturn 0;\n}"},
+	{"bash", "bash script starter", "script.sh", "#!/bin/bash\n\nvar=\"Hello World!\"\n\necho \"$var\""},
+	{"python", "python hello world with main", "main.py", "def main():\n\tprint(\"Hello World!\")\n\nif __name__ == \"__main__\":\n\tmain()"},
+	{"go", "golang hello world", "main.go", "package main\n\nimport \"fmt\"\n\nfunc main() {\n\tfmt.Println(\"Hello World!\")\n}"},
 };
 
 int templates_num = sizeof(templates) / sizeof(struct Template);
@@ -29,7 +33,7 @@ Usage: [OPTION] ... [TEMPLATE_NAME] \n\
 \n\
 \t-h\tPrints out this help message and exits\n\
 \t-v\tPrints out program version\n\
-\t-n\tGive a custom to the generated file\n\
+\t-n\tGive a custom name to the generated file\n\
 \t-o\tDo not create project file, only print it to stdout\n\
 \n\
 Available templates:\n", stdout);
@@ -47,7 +51,10 @@ int main(int argc, char** argv) {
 	if(argc < 2)
 		usage(0);
 
-	FILE* filename = NULL;
+	char* filename = NULL;
+	char* selected = NULL;
+	int no_write = 0;
+	int wanted = -1;
 	int index;
 	int c;
 
@@ -61,27 +68,67 @@ int main(int argc, char** argv) {
 				break;
 			case 'v':
 				PRINT_VERSION
-				break;
+				return 0;
 			case 'o':
-				filename = stdout;
+				no_write = 1;
 				break;
 			case 'n':
-				filename = (FILE*)optarg;
+				filename = optarg;
 				break;
 			case '?':
 				if (optopt == 'n')
-					fprintf (stderr, "A -%c opcionak kell faljnev argumentumot adni\n", optopt);
+					fprintf (stderr, "Option -%c requires an argument\n", optopt);
 				else
-					fprintf (stderr, "Ismeretlen opcio: `\\x%x'.\n", optopt);
+					fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
 				return 1;
 			default:
 				abort ();
 		}
 
-	for (index = optind; index < argc; index++)
-		printf ("Nem opcio a(z): %s\n", argv[index]);
+	selected = argv[optind++];
 
-	
+	if(!selected) {
+		printf("You need to select a template, use -h to print help\n");
+		return 1;
+	}
+
+	for (index = optind; index < argc; index++)
+		printf ("%s is not a valid argument\n", argv[index]);
+
+	for(int i = 0; i < templates_num; i++){
+		if(strcmp(selected, templates[i].name) == 0) {
+			wanted = i;
+			break;
+		}
+	}
+
+	if(wanted == -1) {
+		printf("The %s template is not found\n", selected);
+		return 1;
+	}
+
+
+	if(no_write) {
+		fprintf(stdout, "%s\n", templates[wanted].project_file);
+		return 0;
+	}
+
+	filename = filename == NULL ? templates[wanted].file_name : filename;
+	FILE* file = fopen(filename, "w");
+
+	if(file == NULL) {
+		fprintf(stderr, "Error getting file handle\n");
+		return 1;
+	}
+
+	if(access(filename, W_OK) == 0) {
+		printf("%s already exists\n", filename);
+		return 0;
+	}
+
+	fprintf(file, "%s\n", templates[wanted].project_file);
+
+	fclose(file);
 
 	return 0;
 }
